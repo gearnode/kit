6f43205e812f9d1d5be1c30536dd022cd155fde8
gearnode <bryan@frimin.fr>
2024-11-13 18:04:47 +0400

    Add httpserver wrapper

---
+++ httpserver/handler_wrapper.go
@@ -0,0 +1,302 @@
+// Copyright (c) 2024 Bryan Frimin <bryan@frimin.fr>.
+//
+// Permission to use, copy, modify, and/or distribute this software
+// for any purpose with or without fee is hereby granted, provided
+// that the above copyright notice and this permission notice appear
+// in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+// AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+// CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+// OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+// NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+// CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+package httpserver
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"go.gearno.de/crypto/uuid"
+	"go.gearno.de/kit/internal/version"
+	"go.gearno.de/kit/log"
+	"go.gearno.de/x/panicf"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/propagation"
+	semconv "go.opentelemetry.io/otel/semconv/v1.22.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type (
+	handlerWrapper struct {
+		next            http.Handler
+		requestsTotal   *prometheus.CounterVec
+		requestDuration *prometheus.HistogramVec
+		responseSize    *prometheus.HistogramVec
+		tracer          trace.Tracer
+		logger          *slog.Logger
+	}
+)
+
+const (
+	tracerName = "go.gearno.de/kit/httpserver"
+)
+
+var (
+	internalErrorResponse = map[string]string{
+		"error": "internal error",
+	}
+)
+
+func newHandlerWrapper(
+	next http.Handler,
+	logger *log.Logger,
+	tp trace.TracerProvider,
+	registerer prometheus.Registerer,
+) *handlerWrapper {
+	metricLabels := []string{
+		"method",
+		"host",
+		"flavor",
+		"scheme",
+		"status_code",
+	}
+
+	requestsTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Subsystem: "http_server",
+			Name:      "requests_total",
+			Help:      "Total number of HTTP requests made.",
+		},
+		metricLabels,
+	)
+	registerer.MustRegister(requestsTotal)
+
+	requestDuration := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Subsystem: "http_server",
+			Name:      "request_duration_seconds",
+			Help:      "Duration of HTTP requests in seconds.",
+			Buckets:   prometheus.DefBuckets,
+		},
+		metricLabels,
+	)
+	registerer.MustRegister(requestDuration)
+
+	responseSize := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Subsystem: "http_server",
+			Name:      "response_size_bytes",
+			Help:      "Size of HTTP server responses in bytes",
+			Buckets:   prometheus.ExponentialBuckets(100, 10, 5),
+		},
+		metricLabels,
+	)
+	registerer.MustRegister(responseSize)
+
+	return &handlerWrapper{
+		next: next,
+		tracer: tp.Tracer(
+			tracerName,
+			trace.WithInstrumentationVersion(
+				version.New(0).Alpha(1),
+			),
+		),
+		requestsTotal:   requestsTotal,
+		requestDuration: requestDuration,
+		responseSize:    responseSize,
+	}
+}
+
+func (hw *handlerWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "//health" {
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("{}"))
+		return
+	}
+
+	var (
+		r2        = r.Clone(r.Context())
+		ctx       = r2.Context()
+		start     = time.Now()
+		requestID = r2.Header.Get("x-request-id")
+		ww        = NewWrapResponseWriter(w, r2.ProtoMajor)
+	)
+
+	if requestID == "" {
+		id, err := uuid.NewV7()
+		if err != nil {
+			panicf.Panic("cannot generate request id: %w", err)
+		}
+
+		requestID = id.String()
+	}
+	r2.Header.Set("x-request-id", requestID)
+	ww.Header().Set("x-request-id", requestID)
+
+	var (
+		rootSpan = trace.SpanFromContext(ctx)
+		span     trace.Span
+		logger   = hw.logger.With(
+			log.String("http_request_method", r2.Method),
+			log.String("http_request_scheme", r2.URL.Scheme),
+			log.String("http_request_host", r2.URL.Host),
+			log.String("http_request_path", r2.URL.Path),
+			log.String("http_request_flavor", r2.Proto),
+			log.String("http_request_user_agent", r2.UserAgent()),
+			log.String("http_request_client_ip", r2.RemoteAddr),
+			log.String("http_request_id", requestID),
+		)
+	)
+
+	if rootSpan.IsRecording() {
+		propagator := otel.GetTextMapPropagator()
+		ctx = propagator.Extract(ctx, propagation.HeaderCarrier(r2.Header))
+
+		spanName := fmt.Sprintf("%s %s %s", r2.Method, r2.URL.Host, r2.URL.Path)
+		ctx, span = hw.tracer.Start(
+			ctx,
+			spanName,
+			trace.WithSpanKind(trace.SpanKindServer),
+			trace.WithAttributes(
+				semconv.NetworkPeerAddress(r2.URL.Host),
+				semconv.NetworkPeerPort(atoi(r2.URL.Port())),
+				semconv.URLScheme(r2.URL.Scheme),
+				attribute.String("http.method", r.Method),
+				attribute.String("http.url", r2.URL.String()),
+				attribute.String("http.target", r2.URL.Path),
+				attribute.String("http.host", r2.URL.Host),
+				attribute.String("http.scheme", r2.URL.Scheme),
+				attribute.String("http.flavor", r2.Proto),
+				attribute.String("http.client_ip", r2.RemoteAddr),
+				attribute.String("http.user_agent", r2.UserAgent()),
+				attribute.String("http.request_id", requestID),
+			),
+		)
+		defer span.End()
+	}
+
+	defer func() {
+		duration := time.Since(start)
+
+		// r.Pattern pattern matched by the router can be defined by the sub layer
+
+		hasPanic := false
+		rvr := recover()
+		if rvr != nil {
+			hasPanic = true
+
+			if err, ok := rvr.(error); ok {
+				if rootSpan.IsRecording() {
+					span.RecordError(err)
+					span.SetStatus(codes.Error, err.Error())
+				}
+
+			} else {
+				if rootSpan.IsRecording() {
+					span.SetStatus(codes.Error, fmt.Sprintf("%v", rvr))
+				}
+			}
+
+			ww.WriteHeader(http.StatusInternalServerError)
+			if err := json.NewEncoder(ww).Encode(internalErrorResponse); err != nil {
+				logger.ErrorContext(ctx, "cannot write internal error", log.Error(err))
+			}
+		}
+
+		metricLabels := prometheus.Labels{
+			"method":      r2.Method,
+			"host":        r2.URL.Host,
+			"flavor":      r2.Proto,
+			"scheme":      r2.URL.Scheme,
+			"status_code": strconv.Itoa(ww.Status()),
+		}
+
+		hw.requestsTotal.With(metricLabels).Inc()
+		hw.requestDuration.With(metricLabels).Observe(duration.Seconds())
+		hw.responseSize.With(metricLabels).Observe(estimateRequestSize(r))
+
+		var resSizeString string
+		if ww.BytesWritten() < 1000 {
+			resSizeString = fmt.Sprintf("%dB", ww.BytesWritten())
+		} else if ww.BytesWritten() < 1_000_000 {
+			resSizeString = fmt.Sprintf("%.1fkB", float64(ww.BytesWritten())/1e3)
+		} else if ww.BytesWritten() < 1_000_000_000 {
+			resSizeString = fmt.Sprintf("%.1fMB", float64(ww.BytesWritten())/1e6)
+		} else {
+			resSizeString = fmt.Sprintf("%.1fGB", float64(ww.BytesWritten())/1e9)
+		}
+
+		msg := fmt.Sprintf(
+			"%s %s %d %s %s",
+			r.Method,
+			r.URL.Path,
+			ww.Status(),
+			resSizeString,
+			duration,
+		)
+
+		logger.With(
+			log.Int("http_reponse_size", ww.BytesWritten()),
+			log.Int("http_response_status", ww.Status()),
+		)
+
+		if ww.Status() > 499 && !hasPanic {
+			span.SetStatus(codes.Error, fmt.Sprintf("%d status code", ww.Status()))
+		}
+
+		if ww.Status() > 499 || hasPanic {
+			logger.ErrorContext(ctx, msg)
+		} else {
+			logger.InfoContext(ctx, msg)
+		}
+	}()
+
+	hw.next.ServeHTTP(ww, r)
+}
+
+func atoi(s string) int {
+	v, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+
+	return v
+}
+
+func estimateRequestSize(r *http.Request) float64 {
+	s := 0
+	if r.URL != nil {
+		s = len(r.URL.Path)
+	}
+
+	s += len(r.Method)
+	s += len(r.Proto)
+	for name, values := range r.Header {
+		s += len(name)
+		for _, value := range values {
+			s += len(value)
+		}
+	}
+	s += len(r.Host)
+
+	// NOTE: r.Form and r.MultipartForm are assumed to be included
+	// in r.URL.
+
+	if r.ContentLength != -1 {
+		s += int(r.ContentLength)
+	}
+
+	return float64(s)
+}
---
+++ httpserver/render.go
@@ -0,0 +1,48 @@
+// Copyright (c) 2024 Bryan Frimin <bryan@frimin.fr>.
+//
+// Permission to use, copy, modify, and/or distribute this software
+// for any purpose with or without fee is hereby granted, provided
+// that the above copyright notice and this permission notice appear
+// in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+// AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+// CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+// OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+// NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+// CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+package httpserver
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"go.gearno.de/x/panicf"
+)
+
+func RenderJSON(w http.ResponseWriter, statusCode int, v any) {
+	w.Header().Set("content-type", "application/json; charset=utf-8")
+	w.WriteHeader(statusCode)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		panicf.Panic("cannot json encode value: %w", err)
+	}
+}
+
+func RenderText(w http.ResponseWriter, statusCode int, v string) {
+	w.Header().Set("content-type", "text/plain; charset=ut8")
+	w.WriteHeader(statusCode)
+	w.Write([]byte(v))
+}
+
+func RenderError(w http.ResponseWriter, statusCode int, err error) {
+	response := map[string]string{
+		"error":   strings.ReplaceAll(strings.ToLower(http.StatusText(statusCode)), " ", "_"),
+		"message": err.Error(),
+	}
+
+	RenderJSON(w, statusCode, response)
+}
